5bf4f47359ab8c8fc09084295cb6e8f18ca2cb03
Massimiliano Angelino <massimiliano.angelino@digitalroute.com>
2015-05-18 16:57:16 +0200

    Cleanup

--- hello.js
+++
@@ -1,9 +0,0 @@
-var app = angular.module("MyApp", []);
-
-app.controller("CustomersController", function($scope, $http) {
-  $http.get("http://localhost:5000/").
-    success(function(data) {
-      $scope.names = data;
-    });
-});
-
