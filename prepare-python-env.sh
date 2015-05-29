8149e40d93e2e7653a17820b8a8cc2ede6902954
Massimiliano Angelino <massimiliano.angelino@digitalroute.com>
2015-05-29 09:33:59 +0200

    Python environment preparation script

---
+++ prepare-python-env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo pip install python_dateutil
+sudo pip install flask
+sudo pip install pysimplesoap
+sudo pip install python-dateutil
+sudo pip install simplejson
+sudo pip install requests
